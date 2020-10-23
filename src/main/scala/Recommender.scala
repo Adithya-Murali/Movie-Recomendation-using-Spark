44c0a5cc28a2ee42946fc21b284a017f0ae308f5
Adithya Murali <adithyamurali29@gmail.com>
2020-10-23 23:01:22 +0530

    comments added

--- src/main/scala/Recommender.scala
+++ src/main/scala/Recommender.scala
@@ -30,6 +30,7 @@ object Recommender {
 
     import session.implicits._
 
+//    reading movie data
     val genreArray = udf(genreArrayer _)
     val movieSchema = Encoders.product[MovieData].schema
     val movies = session.read
@@ -38,16 +39,18 @@ object Recommender {
       .csv(moviePath)
       .withColumn("genres",genreArray('genres))
 
+//    reading user ratings
     val ratingSchema = Encoders.product[RatingData].schema
     val ratings = session.read
       .option("header","true")
       .schema(ratingSchema)
       .csv(ratingPath).drop("timestamp")
 
-    // self-join ratings from same user to get all possible movie pair combinations
+//    self-join ratings from same user to get all possible movie pair combinations
     val userMoviePair = ratings.as("r1").join(ratings.as("r2"),
       $"r1.userId" === $"r2.userId" && $"r1.movieId" < $"r2.movieId").drop($"r1.userId").drop($"r2.userId")
 
+//    computing similarity between each pair of movies
     val similarMoviePair = userMoviePair.as[(Int,Double,Int,Double)]
       .groupByKey(x => (x._1,x._3))
       .mapGroups{case(key,value) =>
@@ -59,11 +62,13 @@ object Recommender {
     var coOccurenceThreshold = 50.0
     var again:String = "N"
     do {
+//      Reading movieId form user
       val movieID: Int = readLine("Enter the movieId of a movie you enjoyed: ").toInt
       println("Showing recommendation based on movie: " + movies.filter('movieId === movieID).head().getString(1))
 
       val genreIdentifier = udf(genreIdentityFun _)
 
+//      joining the movie info of each movie in the pair, mainly to utilise genre information
       val movieCols = Seq("movie1", "movie2")
       val pairData = movieCols.foldLeft(similarMoviePair.where('movie1 === movieID || 'movie2 === movieID))((pair, column) =>
         pair
@@ -77,13 +82,14 @@ object Recommender {
       val outer = filteredData.take(10)
       var numRecords = outer.length.toLong
 
+//      Decreasing score threshold in loop until atleast 10 records match the criteria
       while (numRecords < 10) {
         scoreThreshold -= 0.1
         coOccurenceThreshold -= 5
         filteredData = pairData.filter('score > scoreThreshold && 'numPairs > coOccurenceThreshold && 'genreMatch.geq(1))
         numRecords = filteredData.count()
       }
-      //    Using head instead of limit to preserve order
+//      Using head instead of limit to preserve order during foreach
       val recommendations = filteredData.sort('score.desc).head(10)
       var serial = 1
       recommendations.foreach(x => {
@@ -94,12 +100,14 @@ object Recommender {
       })
       println("\nYou waited "+(System.currentTimeMillis()-startTime)/1000+"seconds for the recommendation.")
 
+//      recieving user input to loop
       again = readLine("\nWould you like to get recommendation based on another movieId? (Y/N): ")
       startTime = System.currentTimeMillis()
     } while(again == "Y" || again =="y")
     println("\n\tHope you enjoy the movie!")
 
   }
+//  Function to compute the similarity between each pair of movies by computing score for each pair of ratings
   def computeCosineSimilarity(moviePair:Iterator[(Int,Double,Int,Double)]): (Double, Int) = {
     var numPairs:Int = 0
     var sum_xx:Double = 0.0
@@ -126,6 +134,7 @@ object Recommender {
     (score, numPairs)
   }
 
+//  Converts the genre data into array form
   def genreArrayer(genres: String): Array[Int] = {
     val categories = Seq("Action", "Adventure", "Animation", "Children", "Comedy", "Crime", "Documentary", "Drama", "Fantasy", "Film-Noir", "Horror", "Musical", "Mystery", "Romance", "Sci-Fi", "Thriller", "War", "Western", "IMAX", "(no genres listed)")
     val genreData = Array.fill[Int](19)(0)
@@ -137,11 +146,13 @@ object Recommender {
     genreData
   }
 
+//  Performs AND operation between genres of pair of movies and returns number of intersection
   def genreIdentityFun(gen1: mutable.WrappedArray[Int],gen2: mutable.WrappedArray[Int]):Int = {
     var count:Int = 0
     gen1.foreach(x => if(x == 1 && gen2(gen1.indexOf(x)) == 1) count += 1)
     count
   }
 }
+//Case classes to form schema for data read
 case class RatingData(userId: Int, movieId: Int, rating: Double, timestamp:String)
 case class MovieData(movieId: Int, title: String, genres: String)
